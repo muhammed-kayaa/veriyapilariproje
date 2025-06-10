384c6b9858d4c0c14bf849eef81cb45c7a276de1
muhammed-kayaa <149070657+muhammed-kayaa@users.noreply.github.com>
2025-06-10 16:58:35 +0300

    1.0.6

--- mobile-game-project/app/src/test/java/com/example/mobilegame/ExampleUnitTest.kt
+++ mobile-game-project/app/src/test/java/com/example/mobilegame/ExampleUnitTest.kt
@@ -8,4 +8,43 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun testPushItemToInventory() {
+        val gameEngine = GameEngine()
+        val result = gameEngine.pushItemToInventory("Kılıç")
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun testPopItemFromInventory() {
+        val gameEngine = GameEngine()
+        gameEngine.pushItemToInventory("Kılıç")
+        val removedItem = gameEngine.popItemFromInventory()
+        assertEquals("Kılıç", removedItem)
+    }
+
+    @Test
+    fun testRescueVillage() {
+        val gameEngine = GameEngine()
+        val result = gameEngine.rescueVillage()
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun testUseItem() {
+        val gameEngine = GameEngine()
+        gameEngine.pushItemToInventory("Kılıç")
+        val result = gameEngine.useItem("Kılıç")
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun testSearchItemInInventoryBST() {
+        val gameEngine = GameEngine()
+        gameEngine.pushItemToInventory("Kılıç")
+        gameEngine.addItemToInventoryBST("Kılıç")
+        val result = gameEngine.searchItemInInventoryBST("Kılıç")
+        assertEquals(true, result)
+    }
 }
